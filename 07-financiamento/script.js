8a5261382defb1eea61d5e9cc3c4e78049e46635
EstephanoBorovicz <estephano_borovicz@outlook.com>
2026-04-27 16:59:13 -0300

    exercicio 7, taxa de juros

--- 07-financiamento/script.js
+++ 07-financiamento/script.js
@@ -3,20 +3,19 @@
 function simularFinanciamento(){
 
     let valor = Number(document.getElementById('valor').value)
-    let taxaDeJuros = Number(document.getElementById('taxaDeJuros').value)/100
+    let taxaDeJuros = Number(document.getElementById('taxaDeJuros').value)
     let numeroDeParcelas = Number(document.getElementById('numeroDeParcelas').value)
-
-    let total = valor*(1+taxaDeJuros)**numeroDeParcelas
-
+    let saldoDevedor = valor
+   
     let tabela = document.getElementById("tabelaDeDados")
     tabela.replaceChildren()
 
     for (let index = 1; index <= numeroDeParcelas; index++) {
 
-        let parcela = valor/numeroDeParcelas
-        let jurosDoMes = parcela*taxaDeJuros
-        let totalDoMes = parcela+jurosDoMes
-        let SaldoDevedor = total - (valor*(1+taxaDeJuros)**index)
+        let parcela = (valor/numeroDeParcelas)
+        let jurosDoMes = (parcela*(1+taxaDeJuros/100)**(numeroDeParcelas-index) - parcela)
+        let totalDoMes = parcela + jurosDoMes
+        saldoDevedor = saldoDevedor-parcela
 
         const tableRow = document.createElement("tr")
         const tdMes = document.createElement("td")
@@ -27,9 +26,9 @@ function simularFinanciamento(){
 
         tdMes.textContent = index
         tdParcela.textContent = parcela
-        tdJurosDoMes.textContent = jurosDoMes
-        tdTotalDoMes.textContent = totalDoMes
-        tdSaldoDevedor.textContent = SaldoDevedor.toFixed(2)
+        tdJurosDoMes.textContent = jurosDoMes.toFixed(2)
+        tdTotalDoMes.textContent = totalDoMes.toFixed(2)
+        tdSaldoDevedor.textContent = saldoDevedor.toFixed(2)
 
         tableRow.appendChild(tdMes)
         tableRow.appendChild(tdParcela)
